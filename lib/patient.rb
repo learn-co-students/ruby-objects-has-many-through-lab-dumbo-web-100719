2877c2cb8e2bf44a70d5049abce6aed460ee67b5
RubenV-Dev <r.will16@hotmail.com>
2019-10-13 20:13:15 -0400

    1

--- lib/patient.rb
+++ lib/patient.rb
@@ -24,7 +24,7 @@ class Patient
     end
 
     def doctors
-        appointments.map do |appointment|
+        self.appointments.map do |appointment|
             appointment.doctor
         end
     end
