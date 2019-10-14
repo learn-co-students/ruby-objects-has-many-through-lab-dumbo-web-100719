29e5388169253e4efbcf054c4439610eda0953a8
RubenV-Dev <r.will16@hotmail.com>
2019-10-13 20:06:04 -0400

    1

--- lib/patient.rb
+++ lib/patient.rb
@@ -13,4 +13,14 @@ class Patient
         @@all
     end
 
+    def new_appointment(doctor, date)
+        Appointment.new(date, self, doctor)
+    end
+
+    def appointments
+        Appointment.all.select do |appointment|
+            appointment.patient == self
+        end
+    end
+
 end
