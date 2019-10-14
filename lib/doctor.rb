0a128ae0934dedd79a2132aee83c460e421dba1a
RubenV-Dev <r.will16@hotmail.com>
2019-10-13 20:03:46 -0400

    1

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -22,4 +22,10 @@ class Doctor
     def new_appointment(date, patient)
         Appointment.new(date, patient, self)
     end
+
+    def patients
+        appointments.map do |appointment|
+            appointment.patient
+        end
+    end
 end
