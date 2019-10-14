4a3f716b75e3285c6768675bd11143edde695838
RubenV-Dev <r.will16@hotmail.com>
2019-10-13 20:11:37 -0400

    1

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -20,7 +20,7 @@ class Doctor
     end
 
     def new_appointment(date, patient)
-        Appointment.new(date, patient, self)
+        new_app = Appointment.new(date, patient, self)
     end
 
     def patients
